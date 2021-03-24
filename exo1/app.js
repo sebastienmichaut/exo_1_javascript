809a36248b5e33154d7ca08f80001072384db735
Sébastien Michaut <sebmichaut00@gmail.com>
2021-03-24 23:20:09 +0100

    exo1

---
+++ exo1/app.js
@@ -0,0 +1,28 @@
+var seconde = 60;
+console.log(seconde);
+
+var minute = 20;
+var minute = 30;
+console.log(minute);
+
+var seconde2 = 60
+var minute2 = 30
+console.log(seconde2 * minute2);
+
+nombreTel = 0
+nombreTel ++
+nombreTel++
+nombreTel++
+console.log(nombreTel);
+nombreTel --
+console.log(nombreTel);
+
+var email = 23
+email = email + 5
+console.log(email);
+
+var scoreGame = 420
+scoreGame += 15
+console.log(scoreGame);
+
+var user = 'John'
