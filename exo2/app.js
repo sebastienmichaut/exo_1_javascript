c2756a7bfdff40182bcbf34f4d06793d0be497e1
Sébastien Michaut <sebmichaut00@gmail.com>
2021-03-24 23:37:39 +0100

    exo2

---
+++ exo2/app.js
@@ -0,0 +1,24 @@
+var nom = 'John'
+console.log(nom);
+
+var hello = 'Bonjour comment ça va ? '
+var prenom = 'John'
+console.log(hello + prenom);
+
+var exemple = 'Je travaille en js'
+console.log(exemple.length);
+
+var langage = "JavaScript"
+console.log(langage[0] + langage[4]);
+
+var dessert = "corbeille de fruits frais"
+console.log(dessert.slice(13,19));
+
+var name = 'John Doe'
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+
+var paiement = false
+console.log(paiement);
+var paiement = true
+console.log(paiement);
